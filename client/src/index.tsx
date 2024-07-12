e005ace64d3b553548fad2d164741bd04846f1a4
unknown <MSLadanov@gmail.com>
2024-07-12 10:10:16 +0300

    Creating structure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:'<div>Hello</div>'
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
