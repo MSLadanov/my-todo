a79be76b946b513b621a4271c74570f40f2025ba
Михаил Ладанов <MSLadanov@gmail.com>
2024-07-14 18:04:09 +0300

    Connect with firebase

--- client/src/components/Todos/Todos.tsx
+++ client/src/components/Todos/Todos.tsx
@@ -1,17 +1,12 @@
 import { useEffect, useState } from 'react';
-import { db } from '../../firebase';
+import { db, auth} from '../../firebase';
 import { collection, getDocs } from 'firebase/firestore';
 
 function Todos() {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const querySnapshot = await getDocs(collection(db, 'your-collection')).then((data) => console.log(data));
-    };
+  console.log(auth)
 
-    fetchData();
-  }, []);
   return (
     <div>
       <h1>Todos</h1>
