c82d2a803fb8cb8f966e0f1134136e9f52b7e021
Михаил Ладанов <MSLadanov@gmail.com>
2024-07-14 18:01:56 +0300

    Connect with firebase

--- client/src/components/Todos/Todos.tsx
+++ client/src/components/Todos/Todos.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { db } from '../../firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 function Todos() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, 'your-collection')).then((data) => console.log(data));
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Todos</h1>
--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-declare var process : {
+declare const process : {
     env: {
         REACT_APP_API_KEY: string; 
         REACT_APP_AUTH_DOMAIN: string; 
@@ -16,11 +16,11 @@ declare var process : {
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
