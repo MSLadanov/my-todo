72eb9753038e71a4e06b04fbd7aa53cce098e5d4
unknown <MSLadanov@gmail.com>
2024-07-15 11:38:44 +0300

    Creating sign in / sign up functions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,25 @@
 import React, {useState} from 'react';
 import { Outlet } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
+import { login, logout } from './redux/actions/authActions';
 import './App.css';
 import Header from './components/Header/Header';
 
 function App() {
+  interface IState {
+    user: string
+  }
+  const user = useSelector((state : IState) => state.user)
+  const dispatch = useDispatch()
+  console.log(user)
+  function signIn():void{
+    dispatch(login('John Doe'))
+  }
   const [auth, setAuth] = useState<boolean>(false)
   return (
     <>
-      <button onClick={() => setAuth(prev => !prev)}>Sign In</button>
+      <h1>{user}</h1>
+      <button onClick={() => signIn()}>Sign In</button>
       <Outlet />
       <Header />
     </>
