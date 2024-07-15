9d46aa531e92f9790bb4e32a587ab5661841d0e1
Михаил Ладанов <MSLadanov@gmail.com>
2024-07-15 20:05:34 +0300

    Creating signup / signin form

--- client/src/components/SignIn/SignIn.tsx
+++ client/src/components/SignIn/SignIn.tsx
@@ -1,5 +1,36 @@
+import { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
+
 function SignIn(){
-    return (<div></div>)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    async function handleLogin (e: React.FormEvent<HTMLFormElement>) : Promise<void> {
+        e.preventDefault();
+        try {
+          await signInWithEmailAndPassword(auth, email, password);
+          console.log('User logged in');
+        } catch (error) {
+          console.error('Error logging in:', error);
+        }
+      };
+      return (
+        <form onSubmit={handleLogin}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button type="submit">Sign In</button>
+        </form>
+      );
 }
 
 export default SignIn
--- client/src/components/SignUp/SignUp.tsx
+++ client/src/components/SignUp/SignUp.tsx
@@ -1,5 +1,38 @@
-function SignUp(){
-    return (<div></div>)
-}
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase'
 
-export default SignUp
+function SignUp () {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignUp (e : React.FormEvent<HTMLFormElement>) : Promise<void> {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      console.log('User signed up');
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSignUp}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignUp;
