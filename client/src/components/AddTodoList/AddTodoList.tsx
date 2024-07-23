831248ab8f25f0197a8cc33e5940d36ca7aea1c2
unknown <MSLadanov@gmail.com>
2024-07-23 14:27:57 +0300

    Creating todo list funcs

--- client/src/components/AddTodoList/AddTodoList.tsx
+++ client/src/components/AddTodoList/AddTodoList.tsx
@@ -18,6 +18,19 @@ function AddTodoList(){
             }
         ]
     })
+    function resetFields(){
+        const todos = [{
+            id: uuidv4(),
+            title: '',
+            completed: false
+        },
+        {
+            id: uuidv4(),
+            title: '',
+            completed: false
+        }]
+        setNewTodoList({...newTodoList, todos })
+    }
     function addTodoField(){
         const updatedTodoFields = [...newTodoList.todos, {
             id: uuidv4(),
@@ -27,16 +40,23 @@ function AddTodoList(){
         setNewTodoList({...newTodoList, todos: updatedTodoFields})
     }
     function handleTitleInput(e : React.FormEvent<HTMLInputElement>, id : string){
-        console.log(e.currentTarget.value, id)
+        let todos = newTodoList.todos
+        todos[todos.findIndex((item) => item.id == id)].title = e.currentTarget.value
+        setNewTodoList({...newTodoList, todos })
     }
     function removeTodoField(id: string){
         let todos = newTodoList.todos
         todos = todos.filter((item) => item.id !== id)
         setNewTodoList({...newTodoList, todos })
     }
+    function addTodoList(){
+        console.log(newTodoList)
+        resetFields()
+    }
     return(
     <div>
         <h1>Add todo</h1>
+        <button onClick={addTodoList}>Add TodoList</button>
         <button onClick={addTodoField}>Add todo</button>
         {newTodoList.todos.map((el) => <div key={el.id}><input type="text" onChange={(e) => handleTitleInput(e, el.id)} /><button disabled={newTodoList.todos.length == 1} onClick={() => removeTodoField(el.id)}>&#10060;</button></div>)}
     </div>)
