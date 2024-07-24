a2b7183d5fa09beb8c78d75fc4d12f3af18e363a
unknown <MSLadanov@gmail.com>
2024-07-24 10:52:42 +0300

    Creating userpage

--- client/src/components/UserPage/UserPage.tsx
+++ client/src/components/UserPage/UserPage.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../redux/actions/authActions';
-import { ref } from 'firebase/storage';
+import { ref, getDownloadURL } from 'firebase/storage';
 import { storage } from '../../firebase';
 
 function UserPage() {
@@ -13,11 +13,24 @@ function UserPage() {
   }
   const userId  = useSelector((state : IState) => state.userId)
   const pathReference = ref(storage, `userAvatars/${userId}.jpg`);
+  const [avatar, setAvatar] = useState('')
+  getDownloadURL(pathReference)
+  .then((url) => {
+    setAvatar(url)
+  })
+  .catch((error) => {
+    console.log(error)
+  });
   const dispatch = useDispatch()
   return (
     <div>
       <h1>UserPage</h1>
-      <button onClick={() => dispatch(logout())}>Log Out</button>
+      <div>
+        <img src={avatar} />
+      </div>
+      <div>
+        <button onClick={() => dispatch(logout())}>Log Out</button>
+      </div>
     </div>
   );
 }
