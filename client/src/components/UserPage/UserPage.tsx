316ad33f23d0a9fe7a96e11fc7bb00cb960a6ea7
unknown <MSLadanov@gmail.com>
2024-07-24 10:28:49 +0300

    Creating userpage

--- client/src/components/UserPage/UserPage.tsx
+++ client/src/components/UserPage/UserPage.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../redux/actions/authActions';
 
 function UserPage() {
+  interface IState {
+    displayName: string,
+    email: string,
+    token: string,
+    userId: string | undefined  
+  }
   const dispatch = useDispatch()
+  const userId  = useSelector((state : IState) => state.userId)
   return (
     <div>
       <h1>UserPage</h1>
