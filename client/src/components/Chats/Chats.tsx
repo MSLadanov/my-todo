99993bbc46edbd010b1a22997ddc911b76f31ec7
unknown <MSLadanov@gmail.com>
2024-07-29 13:20:23 +0300

    Creating userpage

--- client/src/components/Chats/Chats.tsx
+++ client/src/components/Chats/Chats.tsx
@@ -1,9 +1,11 @@
 import { getDatabase, ref, child, get} from "firebase/database";
+import { useSelector } from "react-redux";
 
 function Chats (){
+    const userId  = useSelector((state : any) => state.userId)
     const dbRef = ref(getDatabase());
     async function getChatList(){
-        return get(child(dbRef, `chats/a99ff62a-fcf4-454d-8300-830f5c1d3d69`)).then((snapshot) => {
+        return get(child(dbRef, `chatLists/${userId}`)).then((snapshot) => {
           if (snapshot.exists()) {
             const chats = snapshot.val()
             console.log(chats)
