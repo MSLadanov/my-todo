92d959cd128aaf21bf089037667a6fec3021be22
unknown <MSLadanov@gmail.com>
2024-07-29 13:09:58 +0300

    Creating userpage

--- client/src/components/Chats/Chats.tsx
+++ client/src/components/Chats/Chats.tsx
@@ -1,4 +1,17 @@
+import { getDatabase, ref, child, get} from "firebase/database";
+
 function Chats (){
+    const dbRef = ref(getDatabase());
+    async function getChatList(){
+        return get(child(dbRef, `chats/a99ff62a-fcf4-454d-8300-830f5c1d3d69`)).then((snapshot) => {
+          if (snapshot.exists()) {
+            const chats = snapshot.val()
+            console.log(chats)
+        }}).catch((error) => {
+          console.error(error);
+        });
+    }
+    getChatList()
     return (
     <div>
         <h1>Chats</h1>
