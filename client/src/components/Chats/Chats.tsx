1094b08fa46fd97332461bf00ad8229008aac694
unknown <MSLadanov@gmail.com>
2024-07-30 13:40:36 +0300

    Creating chatpage

--- client/src/components/Chats/Chats.tsx
+++ client/src/components/Chats/Chats.tsx
@@ -1,26 +1,33 @@
 import { getDatabase, ref, child, get} from "firebase/database";
 import { useSelector } from "react-redux";
+import { useQuery } from "@tanstack/react-query";
+import Chat from "../Chat/Chat";
 
 function Chats (){
-    const userId  = useSelector((state : any) => state.userId)
+  interface IState {
+    displayName: string,
+    email: string,
+    token: string,
+    userId: string | undefined  
+  }
+  const query = useQuery({ queryKey: ['chats'], queryFn: getChatList })
+    const userId  = useSelector((state : IState) => state.userId)
     const dbRef = ref(getDatabase());
     async function getChatList(){
         return get(child(dbRef, `chatLists/${userId}`)).then((snapshot) => {
           if (snapshot.exists()) {
             const chatsIds = snapshot.val()
-            const chats = chatsIds.map((chatId : any) => {
+            return chatsIds.map((chatId : string) => {
               return get(child(dbRef, `chats/${chatId}`)).then((snapshot) => snapshot.val())
-                    .catch((err) => console.log(err))
-            })
-            console.log(chats)
+                    .catch((err) => console.log(err))})
         }}).catch((error) => {
           console.error(error);
         });
     }
-    getChatList()
     return (
     <div>
         <h1>Chats</h1>
+        {query.data?.map((chat : any) => <Chat></Chat>)}
     </div>)
 }
 
