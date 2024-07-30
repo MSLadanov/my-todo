45bf384ad00dae9b8c43f431a274c43b860e0bf5
unknown <MSLadanov@gmail.com>
2024-07-30 14:20:52 +0300

    Creating chatpage

--- client/src/components/Chats/Chats.tsx
+++ client/src/components/Chats/Chats.tsx
@@ -1,7 +1,9 @@
 import { getDatabase, ref, child, get} from "firebase/database";
 import { useSelector } from "react-redux";
 import { useQuery } from "@tanstack/react-query";
-import Chat from "../Chat/Chat";
+import styled from "styled-components";
+import { Link, useLocation } from 'react-router-dom';
+
 
 function Chats (){
   interface IState {
@@ -17,6 +19,16 @@ function Chats (){
     senderName: string,
     messanges: []
   }
+  const ChatListItem = styled.li`
+
+  `
+  let location = useLocation();
+    let path = ''
+    if (location.pathname.endsWith('/')){
+        path = location.pathname.substring(0, location.pathname.length - 1)
+    } else {
+      path = location.pathname
+    }
   const query = useQuery({ queryKey: ['chats'], queryFn: getChatList })
     const userId  = useSelector((state : IState) => state.userId)
     const dbRef = ref(getDatabase());
@@ -25,16 +37,23 @@ function Chats (){
           if (snapshot.exists()) {
             const chatsIds = snapshot.val()
             return chatsIds.map((chatId : string) => {
-              return get(child(dbRef, `chats/${chatId}`)).then((snapshot) => snapshot.val())
-                    .catch((err) => console.log(err))})
+              return get(child(dbRef, `chats/${chatId}`)).then((snapshot) => {
+                if (snapshot.exists()) {
+                  return snapshot.val();
+                } else {
+                  return [];
+                }
+              }
+            )})
         }}).catch((error) => {
           console.error(error);
         });
     }
+    console.log(query.data)
     return (
     <div>
         <h1>Chats</h1>
-        {query.data?.map((chat : IChat) => <Chat key={chat.id}></Chat>)}
+        <ul>{query.data?.map((chat : IChat) => <ChatListItem key={chat.id}><Link to={`${path}/${chat.id}`} key={chat.id}>{chat.senderName}</Link></ChatListItem>)}</ul>
     </div>)
 }
 
