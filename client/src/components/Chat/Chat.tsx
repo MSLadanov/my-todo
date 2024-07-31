ec8f930ebfb05afa30d0202789e298c6ba582d77
unknown <MSLadanov@gmail.com>
2024-07-31 09:09:58 +0300

    Creating chatpage

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,10 +1,25 @@
 import { useLocation } from "react-router-dom";
+import { getDatabase, ref, child, get} from "firebase/database";
+import { useQuery } from "@tanstack/react-query";
 
 function Chat(){
+    // const query = useQuery({ queryKey: ['chats'], queryFn: getChatData })
     let location = useLocation();
+    const dbRef = ref(getDatabase());
+    const chatId = location.pathname.split('/').at(-1)
+    async function getChatData(id: string) {
+        try {
+            return (await get(child(dbRef, `chats/${id}`))).val()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    if (chatId){
+        console.log(getChatData(chatId))
+    }
     return (
         <div>
-            <h1>Chat {location.pathname}</h1>
+            <h1>Chat</h1>
         </div>
     )
 }
