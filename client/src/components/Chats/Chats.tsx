bdc7971742257d1521ee4cbf1c2da01e38559c04
unknown <MSLadanov@gmail.com>
2024-07-31 08:53:17 +0300

    Creating chatpage

--- client/src/components/Chats/Chats.tsx
+++ client/src/components/Chats/Chats.tsx
@@ -29,27 +29,31 @@ function Chats (){
     } else {
       path = location.pathname
     }
-  // const query = useQuery({ queryKey: ['chats'], queryFn: getChatList })
+  const query = useQuery({ queryKey: ['chats'], queryFn: getChatList })
     const userId  = useSelector((state : IState) => state.userId)
     const dbRef = ref(getDatabase());
-    async function getChatList(){
+    async function getChatsById(chatsIds: string[]): Promise<any[]> {
+      return Promise.all(
+        chatsIds.map(async (chatId: string) => {
+          const chatData = await get(child(dbRef, `chats/${chatId}`));
+          return chatData.val();
+        })
+      );
+    }
+    async function getChatList() {
       try {
-        const chatsIds = (await get(child(dbRef, `chatLists/${userId}`))).val()
-        const chats = chatsIds.map(async (chatId : string) => {
-          const chat = (await get(child(dbRef, `chats/${chatId}`))).val()
-            return chat
-          })
-          return chats
-        }
-       catch (error) {
-        console.log(error)
+        const chatsIdsSnapshot = await get(child(dbRef, `chatLists/${userId}`));
+        const chatsIds = chatsIdsSnapshot.val();
+        return await getChatsById(chatsIds);
+      } catch (error) {
+        console.log(error);
       }
     }
-    console.log(getChatList())
+    console.log(query.data)
     return (
     <div>
         <h1>Chats</h1>
-        {/* <ul>{query.data?.map((chat : IChat) => <ChatListItem key={chat.id}><Link to={`${path}/${chat.id}`} key={chat.id}>{chat.senderName}</Link></ChatListItem>)}</ul> */}
+        <ul>{query.data?.map((chat : IChat) => <ChatListItem key={chat.id}><Link to={`${path}/${chat.id}`} key={chat.id}>{chat.senderName}</Link></ChatListItem>)}</ul>
     </div>)
 }
 
