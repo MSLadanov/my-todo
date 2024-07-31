0f9681508417b1a8d7cdb6dd0d96fdee71aa13df
unknown <MSLadanov@gmail.com>
2024-07-31 10:26:55 +0300

    Creating chatpage

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -2,8 +2,9 @@ import { useLocation } from "react-router-dom";
 import { getDatabase, ref, child, get} from "firebase/database";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import { ref as refStorage, getDownloadURL } from 'firebase/storage';
+import { ref as refStorage, getDownloadURL, listAll } from 'firebase/storage';
 import { storage } from "../../firebase";
+import useAvatar from "../../hooks/useAvatar";
 
 function Chat(){
     const [ receiverAvatar, setReceiverAvatar ] = useState('')
@@ -20,15 +21,37 @@ function Chat(){
         }
     }
     useEffect(() => {
-        getDownloadURL(refStorage(storage, 'userAvatars/no_avatar.jpg'))
-        .then((res) => setReceiverAvatar(res))
-        .catch((err) => console.log(err))
-    },[])
-    console.log(query.data)
-    console.log(receiverAvatar)
+        async function getReceiverAvatar () {
+            const listRef = refStorage(storage, `userAvatars/${query.data.senderId}`);
+            try {
+                const res = await listAll(listRef);
+                const promises = res.items
+                if(promises.length){
+                    getDownloadURL(refStorage(storage, promises[0].fullPath))
+                    .then((res) => setReceiverAvatar(res))
+                    .catch((err) => console.log(err))
+                } 
+                } catch (error) {
+                console.log(error);
+                throw error; 
+                }
+        }
+        if(query.data){
+            getReceiverAvatar()
+        } else {
+            console.log('using no-avatar image')
+            getDownloadURL(refStorage(storage, 'userAvatars/no_avatar.jpg'))
+            .then((res) => setReceiverAvatar(res))
+            .catch((err) => console.log(err))
+        }
+    },[query.data])
+    // console.log(query.data)
+    // console.log(receiverAvatar)
     return (
         <div>
             <h1>Chat</h1>
+            <img src={receiverAvatar} alt="" />
+            {/* <h2>{query.data.senderName}</h2> */}
         </div>
     )
 }
