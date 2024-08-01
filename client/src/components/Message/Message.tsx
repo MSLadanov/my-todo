5c335840703d81921a2dd5717d5f4c642d58accd
unknown <MSLadanov@gmail.com>
2024-08-01 13:15:39 +0300

    Creating chatpage

--- client/src/components/Message/Message.tsx
+++ client/src/components/Message/Message.tsx
@@ -11,19 +11,34 @@ interface MessageProps{
 }
 
 function Message({id, text, timestamp, userId, userName, userAvatar, currentUserId} : MessageProps){
+    const date = new Date(Number(timestamp)).toDateString();
+    const MessageContainer = styled.div`
+        display: flex;
+        justify-content: ${userId !== currentUserId ? 'left' : 'right'}
+    `
     const MessageBox = styled.div`
         display: flex;
-    `
+        width: 70%;
+        justify-content: space-between;
+        flex-direction: ${userId !== currentUserId ? 'row' : 'row-reverse'};
+        margin: 10px;
+        padding: 10px;
+        border-radius: 5px;
+        background-color: ${userId !== currentUserId ? 'chartreuse' : 'aquamarine'};
+    ` 
     const UserImg = styled.img`
         width: 50px;
         height: 50px;
         border-radius: 50%;
     `
     return (
-        <MessageBox>
-            <UserImg src={userAvatar} alt="" />
-            <p>{text}</p>
-        </MessageBox>
+        <MessageContainer>
+            <MessageBox>
+                <UserImg src={userAvatar} alt="" />
+                <p>{text}</p>
+                <p>{date}</p>
+            </MessageBox>
+        </MessageContainer>
     )
 }
 export default Message
