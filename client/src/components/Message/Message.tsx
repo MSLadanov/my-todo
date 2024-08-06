120c725bee1ef30c758056deb315adb99be2f412
Михаил Ладанов <MSLadanov@gmail.com>
2024-08-06 22:36:49 +0300

    styled fix

--- client/src/components/Message/Message.tsx
+++ client/src/components/Message/Message.tsx
@@ -14,30 +14,31 @@ interface MessageProps{
     receiverAvatar: string | undefined,
 }
 
-function Message({id, text, timestamp, userId, userName, userAvatar, currentUserId, senderName, receiverName, senderAvatar, receiverAvatar} : MessageProps){
-    const date = new Date(Number(timestamp)).toDateString();
-    const MessageContainer = styled.div`
+const MessageContainer = styled.div`
         display: flex;
-        justify-content: ${userId === currentUserId ? 'right' : 'left'}
     `
     const MessageBox = styled.div`
         display: flex;
         width: 70%;
         justify-content: space-between;
-        flex-direction: ${userId !== currentUserId ? 'row' : 'row-reverse'};
         margin: 10px;
         padding: 10px;
         border-radius: 5px;
-        background-color: ${userName === receiverName ? 'chartreuse' : 'aquamarine'};
     ` 
     const UserImg = styled.img`
         width: 50px;
         height: 50px;
         border-radius: 50%;
     `
+
+function Message({id, text, timestamp, userId, userName, userAvatar, currentUserId, senderName, receiverName, senderAvatar, receiverAvatar} : MessageProps){
+    const date = new Date(Number(timestamp)).toDateString();
     return (
-        <MessageContainer>
-            <MessageBox>
+        <MessageContainer style={{flexDirection : userId !== currentUserId ? 'row' : 'row-reverse'}}>
+            <MessageBox style={{ 
+                flexDirection : userId !== currentUserId ? 'row' : 'row-reverse',
+                backgroundColor : userName === receiverName ? 'chartreuse' : 'aquamarine'
+            }}>
                 <UserImg src={userName === senderName ? senderAvatar : receiverAvatar} alt="" />
                 <p>{text}</p>
                 <p>{date}</p>
