e8262d0c23477a29a76b575bc92ff52384198cd5
unknown <MSLadanov@gmail.com>
2024-08-08 15:18:31 +0300

    Creating users data route

--- client/src/middleware/chatMiddleware.ts
+++ client/src/middleware/chatMiddleware.ts
@@ -4,7 +4,7 @@ function chatMiddleWare(userId : string | undefined){
     const dbRef = ref(getDatabase());
     async function getUserChats() {
         const chatData = await get(child(dbRef, `chatLists/${userId}`));
-        console.log(chatData)
+        console.log(chatData.val())
     }
     return {getUserChats}
 }
