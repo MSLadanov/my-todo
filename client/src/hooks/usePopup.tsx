686a67bc787e06113aa9b2fd2b3342a9d585458e
Михаил Ладанов <MSLadanov@gmail.com>
2024-08-12 20:22:35 +0300

    creating portal for popups

--- client/src/hooks/usePopup.tsx
+++ client/src/hooks/usePopup.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 
 function usePopup(){
     interface PopupProps{
       text : string
       type: 'success' | 'warning' | 'error',
     }
+    const errorMessage = '#FFCDD2'
+    const warningMessage = '#FFF3CD'
+    const successMessage = '#C8E6C9'
+    const StyledPopup = styled.div`
+      background-color: red;
+     `
     const [ showPopup, setShowPopup ] = useState(false)
     function togglePopup(){
       setShowPopup(true)
@@ -16,13 +23,13 @@ function usePopup(){
     function Popup ({text , type} : PopupProps){
         if(showPopup){
           return ReactDOM.createPortal(
-            <div>
+            <StyledPopup>
               <div/>
               <div>
                   <h1>{text}</h1>
                   <button >Close Popup</button>
               </div>    
-            </div>,
+            </StyledPopup>,
             document.getElementById('portal')!
           )
         } else {
