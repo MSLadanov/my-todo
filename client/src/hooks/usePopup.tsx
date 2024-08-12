0eca124e61cc1073194c022bb341b526e2e3e308
Михаил Ладанов <MSLadanov@gmail.com>
2024-08-12 20:59:51 +0300

    creating portal for popups

--- client/src/hooks/usePopup.tsx
+++ client/src/hooks/usePopup.tsx
@@ -2,31 +2,44 @@ import { useState } from 'react'
 import ReactDOM from 'react-dom'
 import styled from 'styled-components'
 
-const StyledPopup = styled.div`
-      
-`
+const StyledPopup = styled.div<{ isVisible: boolean }>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 12px;
+    height: 35px;
+    padding: 8px 16px;
+    position: relative;
+    bottom: ${({ isVisible }) => (isVisible ? '90px' : '-100px')};
+    left: 50%;
+    transform: translateX(-50%);
+    transition: bottom 0.5s ease-in-out, opacity 0.5s ease-in-out;
+    opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+    z-index: 1000;
+    color: #000;
+`;
 function usePopup(){
     interface PopupProps{
       text : string
       type: 'success' | 'warning' | 'error',
     }
     const colors = {
-      error: '#FFCDD2',
-      warning: '#FFF3CD',
-      success: '#C8E6C9'
+      error: 'rgba(255, 205, 210, 0.8)',
+      warning: 'rgba(255, 243, 205, 0.8)',
+      success: 'rgba(200, 230, 201, 0.8)'
     }
     const [ showPopup, setShowPopup ] = useState(false)
     function togglePopup(){
       setShowPopup(true)
-      setTimeout(() => {
-        setShowPopup(false)
-      }, 2000);
+      // setTimeout(() => {
+      //   setShowPopup(false)
+      // }, 3000);
     }
     function Popup ({text , type} : PopupProps){
         if(showPopup){
           return ReactDOM.createPortal(
-            <StyledPopup style={{'backgroundColor': colors[type]}}>
-              <h1>{text}</h1> 
+            <StyledPopup style={{'backgroundColor': colors[type]}} isVisible={showPopup}>
+              <p>{text}</p> 
             </StyledPopup>,
             document.getElementById('portal')!
           )
