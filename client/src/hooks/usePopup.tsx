bb9b93dd768515ac7ba31752a2467a3b10a06968
Михаил Ладанов <MSLadanov@gmail.com>
2024-08-12 20:02:09 +0300

    creating portal for popups

---
+++ client/src/hooks/usePopup.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+function usePopup(){
+    interface PopupProps{
+      text : string
+    }
+    const [ showPopup, setShowPopup ] = useState(false)
+    function togglePopup(){
+      setShowPopup(true)
+      setTimeout(() => {
+        setShowPopup(false)
+      }, 2000);
+    }
+    function Popup ({text} : PopupProps){
+        if(showPopup){
+          return ReactDOM.createPortal(
+            <div>
+              <div/>
+              <div>
+                  <h1>Popup</h1>
+                  <h1>{text}</h1>
+                  <button >Close Popup</button>
+              </div>    
+            </div>,
+            document.getElementById('portal')!
+          )
+        }
+    }
+    return { togglePopup, Popup }
+}
+
+export default { usePopup }
