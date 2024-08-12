2c777ff854e6f720765de4b1873df5ec593de27c
Михаил Ладанов <MSLadanov@gmail.com>
2024-08-12 19:47:24 +0300

    creating portal for popups

--- client/src/components/Popup/Popup.tsx
+++ client/src/components/Popup/Popup.tsx
@@ -1,7 +1,16 @@
+import ReactDOM from 'react-dom'
+
 function Popup(){
-    return (
-        <div></div>
-    )
+    return ReactDOM.createPortal(
+        <div>
+          <div/>
+          <div>
+              <h1>Popup</h1>
+              <button >Close Popup</button>
+          </div>    
+        </div>,
+        document.getElementById('portal')!
+      )
 }
 
 export default Popup
