dcaf0cbda7c7228b434ac3d5ef004ba15979d365
unknown <MSLadanov@gmail.com>
2024-08-15 13:39:21 +0300

    Creating design

--- client/src/components/Chats/Chats.tsx
+++ client/src/components/Chats/Chats.tsx
@@ -12,10 +12,27 @@ const ChatListItem = styled.li`
     border-radius: 50%;
     height: 50px;
   }
+  display: flex;
   list-style-type: none;
 `
 
 const ChatsBox = styled.div`
+`
+const ChatAvatar = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const ChatInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 15px;
+  & p{
+    color: black;
+    text-decoration: none;
+    margin: 0px;
+  }
 `
 
 function Chats (){
@@ -24,6 +41,12 @@ function Chats (){
     email: string,
     token: string,
     userId: string | undefined  
+  }
+  interface IMessage{
+    id: string,
+    timestamp : string
+    userId: string,
+    userName: string
   }
   interface IChat {
     id: string,
@@ -44,14 +67,21 @@ function Chats (){
   const userId  = useSelector((state : IState) => state.userId)
   const { getChatList } = useChat(userId)
   const query = useQuery({ queryKey: ['chats'], queryFn: getChatList })
+  function showLastMessage(chat : IChat ){
+    return JSON.stringify(chat.messanges.at(-1).userName) + ':' + JSON.stringify(chat.messanges.at(-1).text)
+  }
   console.log(query.data)
   return (
     <ChatsBox>
         <h1>Chats</h1>
-        <ChatList>{query.data?.map((chat : IChat) => <ChatListItem key={chat.id}><Link to={`${path}/${chat.id}`} key={chat.id}>
-          <img src={chat.senderAvatar} alt="" />
-          <p>{JSON.stringify(chat.messanges.at(-1).userName) + ':' + JSON.stringify(chat.messanges.at(-1).text)}</p>
-          {userId === chat.receiverId ? chat.senderName : chat.receiverName}
+        <ChatList>{query.data?.map((chat : IChat) => <ChatListItem key={chat.id}><Link style={{display:'flex'}} to={`${path}/${chat.id}`} key={chat.id}>
+          <ChatAvatar>
+            <img src={chat.senderAvatar} alt="" />
+          </ChatAvatar>
+          <ChatInfo>
+            <p>{userId === chat.receiverId ? chat.senderName : chat.receiverName}</p>
+            <p>{showLastMessage(chat)}</p>
+          </ChatInfo>
         </Link>
         </ChatListItem>)}
         </ChatList>
