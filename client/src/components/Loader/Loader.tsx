c1e4ecc0729bc4d57be16c30b3587517fdf7399a
unknown <MSLadanov@gmail.com>
2024-08-19 13:46:50 +0300

    Creating design

--- client/src/components/Loader/Loader.tsx
+++ client/src/components/Loader/Loader.tsx
@@ -1,5 +1,34 @@
+import styled, { keyframes } from "styled-components";
+
+const LoaderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`
+const spin = keyframes`
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+const Spinner = styled.div`
+  width: 40px;
+  height: 40px;
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-top-color: #000;
+  border-radius: 50%;
+  animation: ${spin} 1s linear infinite;
+`
+
 function Loader() {
-    
+    return (
+        <LoaderWrapper>
+          <Spinner className="spinner"></Spinner>
+          <p>Loading...</p>
+        </LoaderWrapper>
+      );
 }
 
 export default Loader
